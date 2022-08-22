6e4c5a2a77377887a302a6879cde17c243db57d6
Tot3sMalot3s <bkatsevych@icloud.com>
2022-08-22 14:34:41 +0300

    header, navbar

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// MOBILE MENU FUNCTIONALITY
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
+}
+
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+// MOBILE MENU FUNCTIONALITY
